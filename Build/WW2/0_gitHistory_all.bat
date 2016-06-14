d82244db9b4d29ff39dd35770a5eda93f1636a05
kju <pvpscene@web.de>
2016-06-14 20:30:11 +0200

    ~ Added: gitk build script.

---
+++ Build/WW2/0_gitHistory_all.bat
@@ -0,0 +1,54 @@
+cd Assets_c
+gitk
+cd ..
+cd Assets_m
+gitk
+cd ..
+cd Assets_r
+gitk
+cd ..
+cd Assets_s
+gitk
+cd ..
+cd Assets_t
+gitk
+cd ..
+cd Core_a
+gitk
+cd ..
+cd Core_c
+gitk
+cd ..
+cd Core_f
+gitk
+cd ..
+cd Core_m
+gitk
+cd ..
+cd Core_r
+gitk
+cd ..
+cd Core_s
+gitk
+cd ..
+cd Core_t
+gitk
+cd ..
+cd Terrains_c
+gitk
+cd ..
+cd Terrains_m
+gitk
+cd ..
+cd Terrains_r
+gitk
+cd ..
+cd Terrains_s
+gitk
+cd ..
+cd Terrains_t
+gitk
+cd ..
+cd Terrains_w
+gitk
+cd ..
