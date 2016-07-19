2dba2d75e4b7c5dab34bada192304355e866848f
kju <pvpscene@web.de>
2016-07-19 16:12:07 +0200

    ~ Added: build file.

---
+++ Build/WW2/0_gitBash_all.bat
@@ -0,0 +1,54 @@
+cd Assets_c
+"C:\Program Files\Git\git-bash.exe" "--cd=%cd%"
+cd ..
+cd Assets_m
+"C:\Program Files\Git\git-bash.exe" "--cd=%cd%"
+cd ..
+cd Assets_r
+"C:\Program Files\Git\git-bash.exe" "--cd=%cd%"
+cd ..
+cd Assets_s
+"C:\Program Files\Git\git-bash.exe" "--cd=%cd%"
+cd ..
+cd Assets_t
+"C:\Program Files\Git\git-bash.exe" "--cd=%cd%"
+cd ..
+cd Core_a
+"C:\Program Files\Git\git-bash.exe" "--cd=%cd%"
+cd ..
+cd Core_c
+"C:\Program Files\Git\git-bash.exe" "--cd=%cd%"
+cd ..
+cd Core_f
+"C:\Program Files\Git\git-bash.exe" "--cd=%cd%"
+cd ..
+cd Core_m
+"C:\Program Files\Git\git-bash.exe" "--cd=%cd%"
+cd ..
+cd Core_r
+"C:\Program Files\Git\git-bash.exe" "--cd=%cd%"
+cd ..
+cd Core_s
+"C:\Program Files\Git\git-bash.exe" "--cd=%cd%"
+cd ..
+cd Core_t
+"C:\Program Files\Git\git-bash.exe" "--cd=%cd%"
+cd ..
+cd Terrains_c
+"C:\Program Files\Git\git-bash.exe" "--cd=%cd%"
+cd ..
+cd Terrains_m
+"C:\Program Files\Git\git-bash.exe" "--cd=%cd%"
+cd ..
+cd Terrains_r
+"C:\Program Files\Git\git-bash.exe" "--cd=%cd%"
+cd ..
+cd Terrains_s
+"C:\Program Files\Git\git-bash.exe" "--cd=%cd%"
+cd ..
+cd Terrains_t
+"C:\Program Files\Git\git-bash.exe" "--cd=%cd%"
+cd ..
+cd Terrains_w
+"C:\Program Files\Git\git-bash.exe" "--cd=%cd%"
+cd ..
