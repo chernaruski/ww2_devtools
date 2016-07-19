2d01f7c29cb1921f4190847dd1a5933b0885749b
kju <pvpscene@web.de>
2016-07-19 16:11:17 +0200

    ~ Fixed: log file naming.

--- Build/WW2/1_build_Core.bat
+++ Build/WW2/1_build_Core.bat
@@ -7,13 +7,13 @@ rem ----
 REM Core_a
 set _TARGETFOLDER=Core_a
 cd /D "%_SOURCEPATH%\%_TARGETFOLDER%"
-for /D %%i in (*) do ("%_APPPATH%\Makepbo.exe" %_PARAMS% "%_SOURCEPATH%\%_TARGETFOLDER%\%%i" "%_TARGETPATH%\WW2_%_TARGETFOLDER%_%%i" 2>>"%_SOURCEPATH%\BuildPBOs_%_TARGETFOLDER%_.log" 1>nul)
+for /D %%i in (*) do ("%_APPPATH%\Makepbo.exe" %_PARAMS% "%_SOURCEPATH%\%_TARGETFOLDER%\%%i" "%_TARGETPATH%\WW2_%_TARGETFOLDER%_%%i" 2>>"%_SOURCEPATH%\BuildPBOs_%_TARGETFOLDER%.log" 1>nul)
 
 
 REM Core_c
 set _TARGETFOLDER=Core_c
 cd /D "%_SOURCEPATH%\%_TARGETFOLDER%"
-for /D %%i in (*) do ("%_APPPATH%\Makepbo.exe" %_PARAMS% "%_SOURCEPATH%\%_TARGETFOLDER%\%%i" "%_TARGETPATH%\WW2_%_TARGETFOLDER%_%%i" 2>>"%_SOURCEPATH%\BuildPBOs_%_TARGETFOLDER%_.log" 1>nul)
+for /D %%i in (*) do ("%_APPPATH%\Makepbo.exe" %_PARAMS% "%_SOURCEPATH%\%_TARGETFOLDER%\%%i" "%_TARGETPATH%\WW2_%_TARGETFOLDER%_%%i" 2>>"%_SOURCEPATH%\BuildPBOs_%_TARGETFOLDER%.log" 1>nul)
 
 
 REM --- special handling ---
@@ -24,30 +24,30 @@ REM
 REM Core_f
 set _TARGETFOLDER=Core_f
 cd /D "%_SOURCEPATH%\%_TARGETFOLDER%"
-for /D %%i in (*) do ("%_APPPATH%\Makepbo.exe" %_PARAMS% %_ALLOWHPP% "%_SOURCEPATH%\%_TARGETFOLDER%\%%i" "%_TARGETPATH%\WW2_%_TARGETFOLDER%_%%i" 2>>"%_SOURCEPATH%\BuildPBOs_%_TARGETFOLDER%_.log" 1>nul)
+for /D %%i in (*) do ("%_APPPATH%\Makepbo.exe" %_PARAMS% %_ALLOWHPP% "%_SOURCEPATH%\%_TARGETFOLDER%\%%i" "%_TARGETPATH%\WW2_%_TARGETFOLDER%_%%i" 2>>"%_SOURCEPATH%\BuildPBOs_%_TARGETFOLDER%.log" 1>nul)
 
 
 REM Core_m
 set _TARGETFOLDER=Core_m
 cd /D "%_SOURCEPATH%\%_TARGETFOLDER%"
-for /D %%i in (*) do ("%_APPPATH%\Makepbo.exe" %_PARAMS% "%_SOURCEPATH%\%_TARGETFOLDER%\%%i" "%_TARGETPATH%\WW2_%_TARGETFOLDER%_%%i" 2>>"%_SOURCEPATH%\BuildPBOs_%_TARGETFOLDER%_.log" 1>nul)
+for /D %%i in (*) do ("%_APPPATH%\Makepbo.exe" %_PARAMS% "%_SOURCEPATH%\%_TARGETFOLDER%\%%i" "%_TARGETPATH%\WW2_%_TARGETFOLDER%_%%i" 2>>"%_SOURCEPATH%\BuildPBOs_%_TARGETFOLDER%.log" 1>nul)
 
 
 REM Core_r
 set _TARGETFOLDER=Core_r
 cd /D "%_SOURCEPATH%\%_TARGETFOLDER%"
-for /D %%i in (*) do ("%_APPPATH%\Makepbo.exe" %_PARAMS% "%_SOURCEPATH%\%_TARGETFOLDER%\%%i" "%_TARGETPATH%\WW2_%_TARGETFOLDER%_%%i" 2>>"%_SOURCEPATH%\BuildPBOs_%_TARGETFOLDER%_.log" 1>nul)
+for /D %%i in (*) do ("%_APPPATH%\Makepbo.exe" %_PARAMS% "%_SOURCEPATH%\%_TARGETFOLDER%\%%i" "%_TARGETPATH%\WW2_%_TARGETFOLDER%_%%i" 2>>"%_SOURCEPATH%\BuildPBOs_%_TARGETFOLDER%.log" 1>nul)
 
 
 REM Core_s
 set _TARGETFOLDER=Core_s
 cd /D "%_SOURCEPATH%\%_TARGETFOLDER%"
-for /D %%i in (*) do ("%_APPPATH%\Makepbo.exe" %_PARAMS% "%_SOURCEPATH%\%_TARGETFOLDER%\%%i" "%_TARGETPATH%\WW2_%_TARGETFOLDER%_%%i" 2>>"%_SOURCEPATH%\BuildPBOs_%_TARGETFOLDER%_.log" 1>nul)
+for /D %%i in (*) do ("%_APPPATH%\Makepbo.exe" %_PARAMS% "%_SOURCEPATH%\%_TARGETFOLDER%\%%i" "%_TARGETPATH%\WW2_%_TARGETFOLDER%_%%i" 2>>"%_SOURCEPATH%\BuildPBOs_%_TARGETFOLDER%.log" 1>nul)
 
 
 REM Core_t
 set _TARGETFOLDER=Core_t
 cd /D "%_SOURCEPATH%\%_TARGETFOLDER%"
-for /D %%i in (*) do ("%_APPPATH%\Makepbo.exe" %_PARAMS% "%_SOURCEPATH%\%_TARGETFOLDER%\%%i" "%_TARGETPATH%\WW2_%_TARGETFOLDER%_%%i" 2>>"%_SOURCEPATH%\BuildPBOs_%_TARGETFOLDER%_.log" 1>nul)
+for /D %%i in (*) do ("%_APPPATH%\Makepbo.exe" %_PARAMS% "%_SOURCEPATH%\%_TARGETFOLDER%\%%i" "%_TARGETPATH%\WW2_%_TARGETFOLDER%_%%i" 2>>"%_SOURCEPATH%\BuildPBOs_%_TARGETFOLDER%.log" 1>nul)
 
 exit
