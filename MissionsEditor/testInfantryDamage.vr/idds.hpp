4f37025c68a76fb8fe709c0eb2bf7aacbc85820c
kju <pvpscene@web.de>
2016-10-31 11:27:00 +0100

    ~ Fixed: added missing hpp files.

---
+++ MissionsEditor/testInfantryDamage.vr/idds.hpp
@@ -0,0 +1,7 @@
+#define TEST_WeaponSelection_IDD	4711
+#define TEST_InfantrySelection_IDD	4712
+#define TEST_BombSelection_IDD		4713
+#define TEST_GrenadeSelection_IDD	4714
+
+#define TEST_List_IDC			1
+#define TEST_SelectButton_IDC		2
