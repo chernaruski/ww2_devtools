80cf5d2774065d05f628a4851d87e52cad1dcf6c
kju <pvpscene@web.de>
2017-10-19 10:40:06 +0200

    ~ Updated: configuration.

--- Build/WW2/_configureSettings.bat
+++ Build/WW2/_configureSettings.bat
@@ -1,9 +1,17 @@
-set _APPPATH=E:\kju\Programs\MikeroTools
-set _SOURCEPATH=z:\WW2
-set _TARGETPATH=E:\kju\A3Data\WW2\@WW2\addons
+set _APPPATH=D:\kju\Programs\MikeroTools
+set _SOURCEPATH=p:\WW2
+set _TARGETPATH_HQ=D:\kju\release\B\@IFA3_HQ
+set _TARGETPATH_BASE=D:\kju\release\B\@WW2_LITE
+set _TARGETPATH_OBJECTS=D:\kju\release\B\@WW2_OBJECTS_LITE
+set _TARGETPATH_TERRAINSIF=D:\kju\release\B\@WW2_TERRAINS_LITE
+set _TARGETPATH_TERRAINSI44=D:\kju\release\B\@I44_TERRAINS
+set _TARGETPATH_TERRAINSWW2=D:\kju\release\B\@WW2_TERRAINS_LITE
 
-set _PARAMS=-ABUWDP -$
 set _ALLOWHPP=-X=thumbs.db,*.dep,*.cpp,*.bak,*.png,*.log,*.pew
 
-REM activeate for free tools:
-REM set _PARAMS=-ABJUWP -$
+set _PARAMS_STANDARD=-$ -PDABW %_ALLOWHPP%
+rem set _PARAMS_STANDARD=-! -$ -PDABW -Z=default %_ALLOWHPP%
+
+set _PARAMS_MISSIONS=-$ -PDANW %_ALLOWHPP%
+
+set _PARAMS_HQ=-! -$ -PDABW -Z=default
