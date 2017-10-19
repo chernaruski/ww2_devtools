61825ead5f5120d8096ccd70489133761bf4ecd3
kju <pvpscene@web.de>
2017-10-19 09:18:05 +0200

    ~ Removed: obsolete scripts.

--- Build/ww2_i44_terrains/0_gitChangeLog.bat
+++
@@ -1,6 +0,0 @@
-set _FROM=2016-12-01
-set _FILE=WW2_I44_Terains_ChangeLog.txt
-
-echo "START" >> %_FILE%
-
-git log --pretty=format:"%%B" --since==%_FROM% >> %_FILE%
--- Build/ww2_i44_terrains/_configureSettings.bat
+++
@@ -1,9 +0,0 @@
-set _APPPATH=E:\kju\Programs\MikeroTools
-set _SOURCEPATH=z:\ww2_i44_terrains
-set _TARGETPATH=E:\kju\A3Data\WW2\@WW2_I44_Terrains\addons
-
-set _PARAMS=-ABUWDP -$
-set _ALLOWHPP=-X=thumbs.db,*.dep,*.cpp,*.bak,*.png,*.log,*.pew
-
-REM activeate for free tools:
-REM set _PARAMS=-ABJUWP -$
