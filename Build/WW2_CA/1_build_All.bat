65f8125a5a5979ee3098a94c83e62d63fc3a5675
kju <pvpscene@web.de>
2017-10-19 09:16:41 +0200

    ~ Removed: obsolete scripts.

--- Build/WW2_CA/1_build_All.bat
+++
@@ -1,9 +0,0 @@
-rem Prepare
-CALL _configureSettings.bat
-rem ----
-
-cd /D "%_SOURCEPATH%"
-
-for /D %%i in (*) do ("%_APPPATH%\Makepbo.exe" %_PARAMS% "%_SOURCEPATH%\%%i" "%_TARGETPATH%\addons" 2>>"%_SOURCEPATH%\BuildPBOs_WW2_CA.log" 1>nul)
-
-exit
--- Build/WW2_CA/_configureSettings.bat
+++
@@ -1,9 +0,0 @@
-set _APPPATH=E:\kju\Programs\MikeroTools
-set _SOURCEPATH=z:\WW2_CA
-set _TARGETPATH=E:\kju\A3Data\WW2\@WW2_CA
-
-set _PARAMS=-ABUWDP -$
-set _ALLOWHPP=-X=thumbs.db,*.dep,*.cpp,*.bak,*.png,*.log,*.pew
-
-REM activeate for free tools:
-REM set _PARAMS=-ABJUWP -$
