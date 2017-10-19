6f387e8dd81fc0dc3d36a2bbb275990406f4da56
kju <pvpscene@web.de>
2017-10-19 09:16:34 +0200

    ~ Removed: obsolete scripts.

--- Build/CA/_configureSettings.bat
+++
@@ -1,6 +0,0 @@
-set _APPPATH=E:\kju\Programs\MikeroTools
-set _SOURCEPATH=z:\CA
-set _TARGETPATH=E:\kju\A3Data\WW2\@CA\addons
-
-set _PARAMS=-ABJUWP -$
-set _ALLOWHPP=-X=thumbs.db,*.dep,*.cpp,*.bak,*.png,*.log,*.pew
