130f5da9cd24a476e197c5efea48685edd43c078
kju <pvpscene@web.de>
2017-10-19 10:41:43 +0200

    ~ Added: util build scripts.

---
+++ Build/WW2/4_removeModelsRC.bat
@@ -0,0 +1,14 @@
+del p:\WW2\Assets_m\Characters\Americans_m\US_Army\WW2_US_Greatcoat.p3d
+del p:\WW2\Assets_m\Characters\Germans_m\GER_Wehrmacht\WW2_GER_Greatcoat.p3d
+del p:\WW2\Assets_m\Characters\Soviets_m\SOV_RKKA\WW2_SOV_Greatcoat.p3d
+del p:\WW2\Assets_m\Vehicles\Boats_m\WW2_InflatableBoat.p3d
+del p:\WW2\Assets_m\Vehicles\Boats_m\WW2_Sturmboot.p3d
+del p:\WW2\Assets_m\Vehicles\Cars_m\I44_BMW.p3d
+del p:\WW2\Assets_m\Vehicles\Cars_m\I44_BMW_Sidecar.p3d
+del p:\WW2\Assets_m\Vehicles\Tanks_m\I44_PzJager38.p3d
+del p:\WW2\Assets_m\Vehicles\Tanks_m\I44_PzKpfwII_F.p3d
+del p:\WW2\Assets_m\Vehicles\Tanks_m\I44_PzKpfwIV_G.p3d
+del p:\WW2\Assets_m\Vehicles\Tanks_m\I44_SdKfz173.p3d
+del p:\WW2\Assets_m\Vehicles\WheeledAPC_m\I44_SdKfz2.p3d
+
+pause
